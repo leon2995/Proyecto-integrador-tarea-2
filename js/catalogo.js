12ef6b638b558367b238c2d491b1e0f8ada8ae5d
jorgealexis07 <201711104@tese.edu.mx>
2023-01-02 15:03:57 -0600

    Agrego imagenes a las cards

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,16 +1,17 @@
+let itemsContainer= document.getElementById ("CardItems");
 function addItem(item){
     let itemHTML=`
-    <div class="card" id="CardItems" style="width: 18rem;">
-    <div id="carouselExample" class="carousel slide">
+    <div class="card" style="width: 18rem;">
+    <div id="carouselExample" class="carousel slide card-img-top">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="'${item.img1}'" class="d-block w-100" alt="...">
+            <img src="${item.img1}" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="'${item.img2}'" class="d-block w-100" alt="...">
+            <img src="${item.img2}" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="'${item.img3}'" class="d-block w-100" alt="...">
+            <img src="${item.img3}" class="d-block w-100" alt="...">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -21,16 +22,28 @@ function addItem(item){
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
-      </div>
+    </div>
 
     <div class="card-body">
       <h5 class="card-title">${item.title}</h5>
       <p class="card-text">${item.resume}</p>
       <a href="#" class="btn btn-primary">Ver más.</a>
     </div>
-  </div>`
-  let itemsContainer= document.getElementById ("CardItems");
+  </div> `;
   itemsContainer.innerHTML += itemHTML;
 }
-addItem({"'img1'":'fotos/FotosActividades/ActBernal02.jpg','img2':'fotos/FotosActividades/ActBernal01.jpeg','img3':'fotos/FotosActividades/ActBernal03.jpg', 'title': "Bernal", 'resume':'Este pueblo de interesantes construcciones virreinales está asentado a los pies de uno de los peñones más llamativos del continente americano, con vasta vegetación entre las rocas. Todo está ligado a interesantes leyendas e historias que la gente cuenta, en un ambiente provinciano ideal para el descanso.'
-})
+
+//agregar los 10 productos
+addItem({'img1':'https://heraldodemexico.com.mx/u/fotografias/m/2021/7/16/f1280x720-397282_528957_5050.jpg',
+'img2':'https://hoteleseconomicosmexico.com/fotos/queretaro/g-bernal/pueblo-magico-de-bernal-3.jpg',
+'img3':'http://audaznews.com/wp-content/uploads/2022/04/Visita-el-Pueblo-Magico-Pena-de-Bernal-uno-de-los-lugares-mas-concurridos-de-Mexico-2.jpg',
+'title': "Bernal", 
+'resume':'Este pueblo de interesantes construcciones virreinales está asentado a los pies de uno de los peñones más llamativos del continente americano, con vasta vegetación entre las rocas. Todo está ligado a interesantes leyendas e historias que la gente cuenta, en un ambiente provinciano ideal para el descanso.'}
+);
+addItem({'img1':'https://heraldodemexico.com.mx/u/fotografias/m/2021/7/16/f1280x720-397282_528957_5050.jpg',
+'img2':'https://hoteleseconomicosmexico.com/fotos/queretaro/g-bernal/pueblo-magico-de-bernal-3.jpg',
+'img3':'http://audaznews.com/wp-content/uploads/2022/04/Visita-el-Pueblo-Magico-Pena-de-Bernal-uno-de-los-lugares-mas-concurridos-de-Mexico-2.jpg',
+'title': "Poco", 
+'resume':'Este pueblo de interesantes construcciones virreinales está asentado a los pies de uno de los peñones más llamativos del continente americano, con vasta vegetación entre las rocas. Todo está ligado a interesantes leyendas e historias que la gente cuenta, en un ambiente provinciano ideal para el descanso.'}
+);
+
