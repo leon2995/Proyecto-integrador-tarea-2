104fca9caa78906671bd51bd9f9e8dd7f52c4bac
leon2995 <lallo042994@gmail.com>
2023-01-24 21:48:48 -0600

    Se agrega versión 2.5

--- js/navbar.js
+++ js/navbar.js
@@ -1,85 +1,73 @@
 let navBar = document.getElementById("menu");
 
 function menu() {
-  navBar.innerHTML = ` <header class="encabezado">
-  <nav class="navegador">
-
-
-    <a  href="/index.html">
-      <img id="ajolotito" src="/fotos/logo2.png" alt="Link to homepage">
-    </a>
-    <button class="hamburguesa" aria-labelledby="Abrir menú">
-      <i class="fas fa-bars"></i>
+  navBar.innerHTML = ` <section>
+  <!-- Esté es el menú de navegacion navBar -->
+  <nav class="navbar navbar-expand-lg " style="background-color: #1483d3;">
+  
+  <div class="container-fluid container-color">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <ul class="nave-menudo">
-      <li class="nav-menudo-item">
-        <a href="/html/catalogo.html" class="nave-menudo-link navegador-link" >Destinos</a>
-      </li>
-      <li class="nav-menudo-item">
-        <a href="/html/contacto.html" class="nave-menudo-link navegador-link">Contacto</a>
-      </li>
-      <li class="nav-menudo-item">
-        <a href="/html/aboutus.html" class="nave-menudo-link navegador-link">Nosotros</a>
-      </li>
-
-
-      <div class="boton-container ">
-      <span class="spanSesion">SESIÓN</span>
-        <ul>
-            <li>
-                <a href="../html/login.html" class="nave-menudo-link navegador-link">Iniciar sesión</a>
-            </li>
-            <li>
-                <a href="../html/login.html" class="nave-menudo-link navegador-link">Cerrar sesión</a>
-            </li>
-        </ul>
-      </div>
-
-
-      <li class="nav-menudo-item" id="registro">
-        <a href="/html/registro.html" class="nave-menudo-link navegador-link">Registro</a>
-      </li>
-      <li>
-       <a href="/html/carrito.html">
-       <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-cart-plus" viewBox="0 0 16 16">
-  <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-</svg>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="ajolote" href="/index.html">
+        <img id="ajolotito" src="/fotos/logo22.png" alt="Link to homepage" ></a>
+        <!--terminaajolotito-->
+      <ul class="navbar-nav me-auto mb-2 ">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="/html/catalogo.html">Destinos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/html/contacto.html">Contactanos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/html/aboutUs.html">Nosotros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/html/registro.html">Registrarse</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Mi Usuario
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/html/login.html">Iniciar Sesión</a></li>
+            <li><a class="dropdown-item" href="/html/login.html">Cerrar Sesión</a></li>
+          </ul>
+        </li>
+        <li class="navio">
+          <a class="nav-link" href="/html/carrito.html"> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+          <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+          <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+        </svg></a>
+         
+        </li>
+      </ul>
+      
+    </div>
+    <div id="whatsapp">
+      <a href="https://api.whatsapp.com/send?phone=5639049632&text=Hola,%20Bienvenido%20Axolotl%20Adventures%20agradece%20su%20preferencias%20¿En%20qué%20le%20podemos%20ayudar?." target="_blank">
+          <svg width="80" height="80" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+              <g class="layer">
+              <title>Whassap</title>
+              <circle cx="40" cy="40" fill="#fff" id="svg_1" r="28" stroke="#000000" stroke-width="0"/>
+              <path d="m57.81072,21.975c-4.48928,-4.5 -10.46786,-6.975 -16.82142,-6.975c-13.11429,0 -23.78571,10.67143 -23.78571,23.78571c0,4.18928 1.09286,8.28215 3.17143,11.89286l-3.375,12.32142l12.61072,-3.31072c3.47143,1.89642 7.38215,2.89286 11.36786,2.89286l0.01072,0c13.10358,0 24.01072,-10.67143 24.01072,-23.78571c0,-6.35357 -2.7,-12.32142 -7.18928,-16.82142l-0.00001,-0.00001l-0.00001,0l-0.00002,0.00001zm-16.82142,36.6c-3.55714,0 -7.03928,-0.95357 -10.07143,-2.75357l-0.71785,-0.42857l-7.47858,1.96072l1.99286,-7.29642l-0.47143,-0.75c-1.98215,-3.15 -3.02142,-6.78215 -3.02142,-10.52142c0,-10.89642 8.87143,-19.76786 19.77858,-19.76786c5.28215,0 10.24286,2.05714 13.97143,5.79642c3.72857,3.73928 6.02142,8.7 6.01072,13.98215c0,10.90714 -9.09642,19.77858 -19.99286,19.77858l0,-0.00002l-0.00001,0l-0.00001,-0.00001zm10.84286,-14.80714c-0.58928,-0.3 -3.51429,-1.73572 -4.06072,-1.92857c-0.54643,-0.20358 -0.94286,-0.3 -1.33928,0.3c-0.39642,0.6 -1.53214,1.92857 -1.88571,2.33572c-0.34286,0.39642 -0.69642,0.45 -1.28571,0.15c-3.49286,-1.74643 -5.78571,-3.11785 -8.08928,-7.07143c-0.61072,-1.05 0.61072,-0.975 1.74643,-3.24643c0.19286,-0.39642 0.09642,-0.73928 -0.05357,-1.03928c-0.15,-0.3 -1.33928,-3.225 -1.83214,-4.41429c-0.48215,-1.15714 -0.975,-0.99642 -1.33928,-1.01785c-0.34286,-0.02142 -0.73928,-0.02142 -1.13572,-0.02142c-0.39642,0 -1.03928,0.15 -1.58571,0.73928c-0.54643,0.6 -2.07858,2.03572 -2.07858,4.96072c0,2.925 2.13214,5.75357 2.42142,6.15c0.3,0.39642 4.18928,6.39642 10.15714,8.97858c3.77143,1.62857 5.25,1.76786 7.13572,1.48928c1.14643,-0.17143 3.51429,-1.43572 4.00714,-2.82857c0.49286,-1.39286 0.49286,-2.58215 0.34286,-2.82857c-0.13928,-0.26786 -0.53572,-0.41785 -1.125,-0.70714l-0.00001,-0.00001l0.00002,-0.00001l-0.00002,-0.00001z" fill="currentColor" id="svg_2"/>
+              </g>
+              </svg>
       </a>
-      </li>
-
-
-    </ul>
-    
-
-
+    </div>
+    <!--terminachat-->
+  </div>
+  
   </nav>
-</header>
+  
+  </section>
 `;
 }
-/*<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
-  <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-</svg>
- <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"  fill="white" class=" bi-airplane-engines" viewBox="0 0 16 16">
-  <path d="M8 0c-.787 0-1.292.592-1.572 1.151A4.347 4.347 0 0 0 6 3v3.691l-2 1V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.191l-1.17.585A1.5 1.5 0 0 0 0 10.618V12a.5.5 0 0 0 .582.493l1.631-.272.313.937a.5.5 0 0 0 .948 0l.405-1.214 2.21-.369.375 2.253-1.318 1.318A.5.5 0 0 0 5.5 16h5a.5.5 0 0 0 .354-.854l-1.318-1.318.375-2.253 2.21.369.405 1.214a.5.5 0 0 0 .948 0l.313-.937 1.63.272A.5.5 0 0 0 16 12v-1.382a1.5 1.5 0 0 0-.83-1.342L14 8.691V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v.191l-2-1V3c0-.568-.14-1.271-.428-1.849C9.292.591 8.787 0 8 0ZM7 3c0-.432.11-.979.322-1.401C7.542 1.159 7.787 1 8 1c.213 0 .458.158.678.599C8.889 2.02 9 2.569 9 3v4a.5.5 0 0 0 .276.447l5.448 2.724a.5.5 0 0 1 .276.447v.792l-5.418-.903a.5.5 0 0 0-.575.41l-.5 3a.5.5 0 0 0 .14.437l.646.646H6.707l.647-.646a.5.5 0 0 0 .14-.436l-.5-3a.5.5 0 0 0-.576-.411L1 11.41v-.792a.5.5 0 0 1 .276-.447l5.448-2.724A.5.5 0 0 0 7 7V3Z"/>
-</svg>*/
+
 menu();
 
 
-
-const hambur = document.querySelector(".hamburguesa");
-const naveM = document.querySelector(".nave-menudo");
-
-hambur.addEventListener("click", () => {
-  naveM.classList.toggle("nave-menudo_visible");
-
-  if (naveM.classList.contains("nave-menudo_visible")) {
-    hambur.setAttribute("aria-labelledby", "Cerrar menú");
-  } else {
-    hambur.setAttribute("aria-labelledby", "Abrir menú");
-  }
-});
 
 
 
