93423b56ab1391aabb376c85bcacb47ce56fd728
jorgealexis07 <201711104@tese.edu.mx>
2023-01-26 17:27:45 -0600

    Arregle la posicion del footer

--- js/footer.js
+++ js/footer.js
@@ -6,7 +6,6 @@ function pieDePag() {
     </a>
     <span class="mb-3 mb-md-0 text-white"class="text-white">&copy; 2023 Ajolote Adventure, Inc</span>
   </div>
-
   <ul class="nav col-md-6 justify-content-center list-unstyled d-flex">
       <!-- Agregar -->
     <li class="nav-item1"><a href="/html/addProducto.html" class="nav-link1 px-2 link-fu">Añadir productos</a></li>
@@ -20,6 +19,7 @@ function pieDePag() {
         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
       </svg></a></li>
   </ul>
-</footer>`;
+</footer>
+`;
 }
 pieDePag();
