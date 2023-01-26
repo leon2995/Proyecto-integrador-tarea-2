8eed52e5a06806d00c7c2cb44f1ea9b63d88de59
jorgealexis07 <201711104@tese.edu.mx>
2023-01-25 21:36:33 -0600

    Termino formulario de registro

--- js/registro.js
+++ js/registro.js
@@ -1,74 +1,112 @@
-//Variables globales
-let btnEnviar = document.getElementById('btn-enviar');
-let dataUser = JSON.parse(localStorage.getItem('dataUser')) || [];
+let btnEnviar = document.getElementById("btn-enviar");
+let dataUser = JSON.parse(localStorage.getItem("dataUser")) || [];
 
 // Validaciones
 function validaNombre(nombre) {
-  return nombre.trim().replace(/ /g, '').length >= 3;
+  return nombre.trim().replace(/ /g, "").length >= 3;
 }
 function validaCorreo(correo) {
-    let email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return correo.match(email) !== null;
+  let email =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return correo.match(email) !== null;
 }
 function validaTelefono(telefono) {
   let telefonoRegex = /^\d{10}$/;
   return telefono.match(telefonoRegex) !== null;
 }
 function validaPassword(password) {
-  let passwordRegex = /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/;
+  let passwordRegex =
+    /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/;
   return password.match(passwordRegex) !== null;
 }
 
 // Manejo de eventos
-btnEnviar.addEventListener('click', function (event) {
-    event.preventDefault();
-    let inputNombre = document.getElementById('nombre');
-    let inputEmail = document.getElementById('correo');
-    let inputTel = document.getElementById('telefono');
-    let inputPassword = document.getElementById('password');
-    let alertError = document.getElementById('alert-error');
-    let alertSuccess = document.getElementById('alert-success');
+btnEnviar.addEventListener("click", function (event) {
+  event.preventDefault();
+  let inputNombre = document.getElementById("nombre");
+  let inputEmail = document.getElementById("correo");
+  let inputTel = document.getElementById("telefono");
+  let inputPassword = document.getElementById("password");
+  let inputRPassword = document.getElementById("Rpassword");
+  let alertError = document.getElementById("alert-error");
+  let alertSuccess = document.getElementById("alert-success");
+  alertError.style.display = "none";
+  alertError.innerHTML = "";
 
-    alertError.style.display = 'none';
-    alertError.innerHTML = '';
+  if (
+    validaNombre(inputNombre.value) &&
+    validaCorreo(inputEmail.value) &&
+    validaTelefono(inputTel.value) &&
+    validaPassword(inputPassword.value) &&
+    inputPassword.value == inputRPassword.value
+  ) {
+    // Agregar los datos del usuario al arreglo
+    dataUser.push({
+      nombre: inputNombre.value,
+      correo: inputEmail.value,
+      telefono: inputTel.value,
+      password: inputPassword.value,
+    });
 
-    if (validaNombre(inputNombre.value) &&
-        validaCorreo(inputEmail.value) &&
-        validaTelefono(inputTel.value) &&
-        validaPassword(inputPassword.value)) {
-            // Agregar los datos del usuario al arreglo
-            dataUser.push({
-              nombre: inputNombre.value,
-              correo: inputEmail.value,
-              telefono: inputTel.value,
-              password: inputPassword.value
-          });
-  
-          // Almacenar el objeto en el LocalStorage
-          localStorage.setItem('dataUser', JSON.stringify(dataUser));
-  
-          // Mostrar mensaje de éxito
-          alertSuccess.innerHTML = "Registro exitoso";
-          alertSuccess.style.display = 'block';
-          inputNombre.value = '';
-          inputEmail.value = '';
-          inputTel.value = '';
-          inputPassword.value = '';
-      } else {
-          // Mostrar mensajes de error
-          if (!validaNombre(inputNombre.value)) {
-              alertError.innerHTML += 'El nombre debe contener 3 caracteres o más.';
-          }
-          if (!validaCorreo(inputEmail.value)) {
-              alertError.innerHTML += 'El correo electrónico no es válido.';
-          }
-          if (!validaTelefono(inputTel.value)) {
-              alertError.innerHTML += 'El teléfono no es válido.';
-          }
-          if (!validaPassword(inputPassword.value)) {
-              alertError.innerHTML += 'La contraseña no es válida.';
-          }
-          alertError.style.display = 'block';
-      }
-      
-  });
+    // Almacenar el objeto en el LocalStorage
+    localStorage.setItem("dataUser", JSON.stringify(dataUser));
+
+    // Mostrar mensaje de éxito
+    //alertSuccess.innerHTML = "Registro exitoso";
+    //alertSuccess.style.display = 'block';
+    inputNombre.style.border = "solid green 2px";
+    inputEmail.style.border = "solid green 2px";
+    inputTel.style.border = "solid green 2px";
+    inputPassword.style.border = "solid green 2px";
+    inputRPassword.style.border = "solid green 2px";
+    Swal.fire({
+      title: "¡Registro exitoso!",
+      icon: "success",
+      confirmButtonClass: "custom-confirm-button-class",
+    });
+    inputNombre.value = "";
+    inputEmail.value = "";
+    inputTel.value = "";
+    inputPassword.value = "";
+    inputRPassword.value = "";
+    inputNombre.style.border = "";
+    inputEmail.style.border = "";
+    inputTel.style.border = "";
+    inputPassword.style.border = "";
+    inputRPassword.style.border = "";
+  } else {
+    // Mostrar mensajes de error
+    if (!validaNombre(inputNombre.value)) {
+      alertError.innerHTML += "El nombre debe contener 3 caracteres o más.";
+      inputNombre.style.border = "solid red 2px";
+    }
+    if (!validaCorreo(inputEmail.value)) {
+      alertError.innerHTML += "<br/>El correo electrónico no es válido.";
+      inputEmail.style.border = "solid red 2px";
+    }
+    if (!validaTelefono(inputTel.value)) {
+      alertError.innerHTML += "<br/>El teléfono no es válido.";
+      inputTel.style.border = "solid red 2px";
+    }
+    if (!validaPassword(inputPassword.value)) {
+      alertError.innerHTML += "<br/>La contraseña no es válida.";
+      inputPassword.style.border = "solid red 2px";
+    }
+    if (!validaPassword(inputRPassword.value)) {
+      alertError.innerHTML +=
+        "<br/>La contraseña y su confirmación deben ser iguales y cumplir con los requisitos de seguridad.";
+      inputRPassword.style.border = "solid red 2px";
+    }
+    setTimeout(() => {
+      alertError.innerHTML = "";
+      alertError.style.display = "none";
+      inputNombre.style.border = "";
+      inputEmail.style.border = "";
+      inputTel.style.border = "";
+      inputPassword.style.border = "";
+      inputRPassword.style.border = "";
+    }, 2000);
+
+    alertError.style.display = "block";
+  }
+});
